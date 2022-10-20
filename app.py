912d48c596b259b59ecf6dd696dbe4ca1ee8d60a
Irene Nguyen <irenenguyen1017@gmail.com>
2022-10-20 21:49:21 +1100

    test linting

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# combined with `ordered = True` to make the names of the keys sorted in the correct setting order.
 app.config["JSON_SORT_KEYS"] = False
 
 ##### Set the database URI via SQLAlchemycome.
